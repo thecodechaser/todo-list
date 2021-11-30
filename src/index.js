38de7893b6f472f8677301f4c5be21aa92a3d0c2
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-30 19:18:16 +0530

    Add array for list items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import _ from "lodash";
 import "./style.css";
 
+const items = [
+  {
+    description: "Doctor appointment at 10AM",
+    completed: false,
+    index: 0,
+  },
+  {
+    description: "Visit friends home at 12PM",
+    completed: false,
+    index: 1,
+  },
+  {
+    description: "Complete project for Microverse",
+    completed: false,
+    index: 2,
+  },
+  {
+    description: "Take programming tutorials on udemy",
+    completed: false,
+    index: 3,
+  },
+  {
+    description: "Dinner plan with family at hotel",
+    completed: false,
+    index: 4,
+  },
+];
