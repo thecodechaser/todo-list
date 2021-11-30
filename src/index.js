1972d62cd09cb33abeccbfee58e8c4c5d870ca26
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-11-30 20:44:40 +0530

    Modify files

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import _ from "lodash";
-import "./style.css";
-import {displayTask} from './utils/utils.js';
+import _ from 'lodash';
+import './style.css';
+import { displayTask } from './utils/utils.js';
 
 export const items = [
   {
-    description: "Doctor appointment at 10AM",
+    description: 'Doctor appointment at 10AM',
     completed: false,
     index: 0,
   },
   {
-    description: "Visit friends home at 12PM",
+    description: 'Visit friends home at 12PM',
     completed: false,
     index: 1,
   },
   {
-    description: "Complete project for Microverse",
+    description: 'Complete project for Microverse',
     completed: false,
     index: 2,
   },
   {
-    description: "Take programming tutorials on udemy",
+    description: 'Take programming tutorials on udemy',
     completed: false,
     index: 3,
   },
   {
-    description: "Dinner plan with family at hotel",
+    description: 'Dinner plan with family at hotel',
     completed: false,
     index: 4,
   },
